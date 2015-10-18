1a62e50875c788ef4d387758305964a1d2b33750
Robert Stoll <rstoll@tutteli.ch>
2015-10-18 18:40:52 +0200

    - adding symfony files - already set up a login - enabled doctrine migrations

---
+++ src/Tutteli/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tutteli\AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'TutteliAppBundle:Security:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+}
---
+++ src/Tutteli/AppBundle/TutteliAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tutteli\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TutteliAppBundle extends Bundle
+{
+}
