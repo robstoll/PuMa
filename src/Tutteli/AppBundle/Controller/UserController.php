cbd8e7b384533ab1307ea4ca87eb339850d164d2
Robert Stoll <rstoll@tutteli.ch>
2015-10-19 18:22:10 +0200

    added an agnostic controller for user management

---
+++ src/Tutteli/AppBundle/Controller/UserController.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the project tutteli/purchase published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit https://github.com/robstoll/purchase
+ */
+
+namespace Tutteli\AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\HttpFoundation\Request;
+use Tutteli\AppBundle\Entity\User;
+use Tutteli\AppBundle\Form\UserType;
+
+class UserController extends FOSRestController implements ClassResourceInterface {
+
+    public function cgetAction() {
+        $repository = $this->getDoctrine()->getRepository('TutteliAppBundle:User');
+        $data = $repository->findAll();
+        $view = $this->view($data, 200)
+            ->setTemplate("TutteliAppBundle:User:cget.html.twig")
+            ->setTemplateVar('users');
+        return $this->handleView($view);
+    }
+
+    public function newAction() {
+        return $this->renderUserForm($this->getForm());
+    }
+
+    private function getForm(User $user = null) {
+        return $this->createForm(new UserType(), $user, ['action' => $this->generateUrl('post_users')]);
+    }
+
+    private function renderUserForm(\Symfony\Component\Form\Form $form) {
+        $view = $this->view($form, 200)
+            ->setTemplate("TutteliAppBundle:User:new.html.twig")
+            ->setTemplateVar("form");
+        return $this->handleView($view);
+    }
+
+    public function cpostAction(Request $request) {
+        $user = new User();
+        $form = $this->createForm(new UserType(), $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectView(
+                    $this->generateUrl('get_user', array('id' => $user->getId())), Codes::HTTP_CREATED);
+
+        }
+
+        return $this->renderUserForm($form);
+    }
+
+    public function getAction($id) {
+        $repository = $this->getDoctrine()->getRepository('TutteliAppBundle:User');
+        $data = $repository->findAll();
+        $view = $this->view($data, 200)
+            ->setTemplate("TutteliAppBundle:User:cget.html.twig")
+            ->setTemplateVar('users');
+        return $this->handleView($view);
+    }
+
+    public function editUserAction($slug) {
+        //TODO
+    }
+
+    public function putUserAction($id) {
+        //TODO
+    }
+
+
+    public function patchUserAction($slug) {
+        
+    }
+
+    // "remove_user"   [GET] /users/{slug}/remove
+
+    public function deleteUserAction($slug) {
+        
+    }
+
+    public function linkUserAction($slug) {
+        
+    }
+
+// "link_user_friend"     [LINK] /users/{slug}
+
+    public function unlinkUserAction($slug) {
+        
+    }
+
+// "unlink_user_friend"     [UNLINK] /users/{slug}
+}
--- src/Tutteli/AppBundle/Entity/User.php
+++ src/Tutteli/AppBundle/Entity/User.php
@@ -10,24 +10,37 @@ namespace Tutteli\AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Table(name="app_users")
  * @ORM\Entity(repositoryClass="Tutteli\AppBundle\Entity\UserRepository")
+ * @ExclusionPolicy("all")
+ * @UniqueEntity("username")
  */
-class User implements UserInterface, \Serializable
-{
+class User implements UserInterface, \Serializable {
+
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Expose
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=25, unique=true)
+     * @Expose
      */
     private $username;
+
+    /**
+     * @Assert\Regex(pattern="/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{7,}/", message="val.password")
+     */
+    private $plainPassword;
 
     /**
      * @ORM\Column(type="string", length=64)
@@ -36,53 +49,49 @@ class User implements UserInterface, \Serializable
 
     /**
      * @ORM\Column(type="string", length=60, unique=true)
+     * @Expose
+     * @Assert\Email(checkMX=true)
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=20, nullable=true)
+     * @Expose
      */
     private $role;
-    
-    public function __construct()
-    {
-        $this->isActive = true;
+
+    public function __construct() {
         // may not be needed, see section on salt below
         // $this->salt = md5(uniqid(null, true));
     }
-
-    public function getUsername()
-    {
+    
+    public function getUsername() {
         return $this->username;
     }
 
-    public function getSalt()
-    {
+    public function getSalt() {
         // you *may* need a real salt depending on your encoder
         // see section on salt below
         return null;
     }
 
-    public function getPassword()
-    {
+    public function getPassword() {
         return $this->password;
     }
 
-    public function getRoles()
-    {
-        if ($this->role != null){
+    public function getRoles() {
+        if ($this->role != null) {
             return [$this->role];
         }
         return ['ROLE_USER'];
     }
 
-    public function eraseCredentials()
-    {
+    public function eraseCredentials() {
+        
     }
 
     /** @see \Serializable::serialize() */
-    public function serialize()
-    {
+    public function serialize() {
         return serialize(array(
             $this->id,
             $this->username,
@@ -93,15 +102,14 @@ class User implements UserInterface, \Serializable
     }
 
     /** @see \Serializable::unserialize() */
-    public function unserialize($serialized)
-    {
+    public function unserialize($serialized) {
         list (
             $this->id,
             $this->username,
             $this->password,
             // see section on salt below
             // $this->salt
-        ) = unserialize($serialized);
+            ) = unserialize($serialized);
     }
 
     /**
@@ -109,8 +117,7 @@ class User implements UserInterface, \Serializable
      *
      * @return integer
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
@@ -121,8 +128,7 @@ class User implements UserInterface, \Serializable
      *
      * @return User
      */
-    public function setUsername($username)
-    {
+    public function setUsername($username) {
         $this->username = $username;
 
         return $this;
@@ -135,8 +141,7 @@ class User implements UserInterface, \Serializable
      *
      * @return User
      */
-    public function setPassword($password)
-    {
+    public function setPassword($password) {
         $this->password = $password;
 
         return $this;
@@ -149,8 +154,7 @@ class User implements UserInterface, \Serializable
      *
      * @return User
      */
-    public function setEmail($email)
-    {
+    public function setEmail($email) {
         $this->email = $email;
 
         return $this;
@@ -161,8 +165,38 @@ class User implements UserInterface, \Serializable
      *
      * @return string
      */
-    public function getEmail()
-    {
+    public function getEmail() {
         return $this->email;
     }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     *
+     * @return User
+     */
+    public function setRole($role) {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole() {
+        return $this->role;
+    }
+
+    public function getPlainPassword() {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($newPassword) {
+        $this->plainPassword = $newPassword;
+    }
+
 }
