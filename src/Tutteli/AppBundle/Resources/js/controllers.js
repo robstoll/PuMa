14938c0360e82abd430da8efdd8de64be28d9712
Robert Stoll <rstoll@tutteli.ch>
2015-10-30 09:56:11 +0100

    included angular-pre-work
    - included the angular-pre-work to enable that users can fill in the login form before angular was loaded
    - adapted symfony templates accordingly

--- src/Tutteli/AppBundle/Resources/js/controllers.js
+++ src/Tutteli/AppBundle/Resources/js/controllers.js
@@ -1,19 +1,34 @@
 /* 
- * This file is part of the project tutteli/purchase published under the Apache License 2.0
+ * This file is part of the project tutteli-purchase published under the Apache License 2.0
  * For the full copyright and license information, please have a look at LICENSE in the
  * root folder or visit https://github.com/robstoll/purchase
  */
+'use strict';
 
-var tutteliPurchaseCtrls = angular.module('tutteli-purchase.ctrls', []);
-
-tutteliPurchaseCtrls.controller('tutteliLoginModalCtrl', function ($scope, tutteliLoginApi) {
+angular.module('tutteli.ctrls', ['tutteli.preWork'])
+  .controller('tutteli.LoginCtrl', 
+    ['$scope', '$rootScope', '$cookies', 'tutteli.PreWork',
+    function ($scope, $rootScope, $cookies, PreWork) {
+    
+        $scope.credentials = {
+                username: '',
+                password: '',
+                csrf_token: ''
+        };
+        
+        PreWork.merge('login.tpl', $scope);        
+        
+        $scope.login = function (credentials, $event) {
+            //TODO
+        };
+    }
+]).controller('tutteli.LoginModalCtrl',  
+  ['$scope', function ($scope) {
 
     this.cancel = $scope.$dismiss;
 
-    this.submit = function (username, password) {
-      tutteliLoginApi.login(username, password).then(function (user) {
-        $scope.$close(user);
-      });
+    this.submit = function (credentials) {
+        //TODO
     };
-
-});
+  }
+]);
