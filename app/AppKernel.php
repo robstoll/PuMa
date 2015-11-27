7ef412269ca033e89bc3599c5b9a6af4c082bae7
Robert Stoll <rstoll@tutteli.ch>
2015-11-27 10:41:01 +0100

    removed purchase-bundle
    I think it is over-engineered to have an own bundle. It won't be reused most probably

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Tutteli\AppBundle\TutteliAppBundle(),
-            new Tutteli\PurchaseBundle\TutteliPurchaseBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
         );
