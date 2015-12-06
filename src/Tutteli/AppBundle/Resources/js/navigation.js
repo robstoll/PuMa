1127d36ac4f1207300ef4a03b6344b80446411e5
Robert Stoll <rstoll@tutteli.ch>
2015-12-06 15:06:01 +0100

    added navigation controller
    - added a AngularJS navigation controller
    - layout does now always contain the navigation and only hides it per default (if not logged in etc.)

---
+++ src/Tutteli/AppBundle/Resources/js/navigation.js
@@ -0,0 +1,27 @@
+/* 
+ * This file is part of the project tutteli-purchase published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit https://github.com/robstoll/purchase
+ */
+(function(){
+'use strict';
+
+angular.module('tutteli.navigation', [])
+    .controller('tutteli.NavigationController', NavigationController);
+
+NavigationController.$inject = ['tutteli.auth.AuthService', 'tutteli.auth.USER_ROLES', 'tutteli.auth.Session'];
+function NavigationController(AuthService, ROLES, Session) {
+    this.isAuthenticated = AuthService.isAuthenticated;
+    this.isAdmin = function() {
+        return AuthService.isAuthorised(ROLES.admin);
+    };
+    
+    this.username = function() {
+        if (Session.user) {
+            return Session.user.username;
+        }
+        return 'anon';
+    };
+}
+
+})();
