db6695faf315bc29e4c1687821d7b0a29aeb186a
Robert Stoll <rstoll@tutteli.ch>
2016-03-28 16:36:23 +0200

    improved the backup script
    uses gzip now and deletes the files after 2 months

--- app/scripts/backupMysql.sh
+++ app/scripts/backupMysql.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 password=`cat /home/httpd/vhosts/tutteli.ch/private/purchase/app/scripts/password.txt`
-mysqldump --user=purchase --password=$password --hex-blob -n --all-databases > /home/httpd/vhosts/tutteli.ch/private/backups/purchase-`/bin/date +\%Y\%m\%d`.sql
-find /home/httpd/vhosts/tutteli.ch/private/backups/purchase-*.sql -mtime +120 -exec rm {} \;
+mysqldump --user=purchase --password=$password --hex-blob -n --all-databases | gzip > /home/httpd/vhosts/tutteli.ch/private/backups/purchase-`/bin/date '+%Y-%m-%d_%H-%M'`.sql.gz
+find /home/httpd/vhosts/tutteli.ch/private/backups/purchase-*.sql.gz -mtime +60 -exec rm {} \;
