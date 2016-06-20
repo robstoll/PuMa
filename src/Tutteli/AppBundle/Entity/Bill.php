0ed8aa2b75abca99443a9e59940e770b267354c0
Robert Stoll <rstoll@tutteli.ch>
2016-06-20 12:34:18 +0200

    change tutteli-puma to tutteli/puma
    The file header was sometimes tutteli-puma instead of tutteli/puma. Changed it.

--- src/Tutteli/AppBundle/Entity/Bill.php
+++ src/Tutteli/AppBundle/Entity/Bill.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the project tutteli-puma published under the Apache License 2.0
+ * This file is part of the project tutteli/puma published under the Apache License 2.0
  * For the full copyright and license information, please have a look at LICENSE in the
  * root folder or visit https://github.com/robstoll/PuMa
  */
